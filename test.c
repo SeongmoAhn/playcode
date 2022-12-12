4195d3ffe7fd4202ed7b585ef6b6a9a58183f068
SeongmoAhn <asm0619@gmail.com>
2022-12-12 20:54:50 +0900

    곱셈과 나눗셈도 추가

--- test.c
+++ test.c
@@ -2,7 +2,10 @@
 int main(void)
 {
     int a = 10, b = 20;
+    double c;
     printf("%d\n", a + b);
     printf("%d\n", a - b);
+    printf("%f\n", a * b);
+    printf("%f\n", a / b);
     return 0;
 }
