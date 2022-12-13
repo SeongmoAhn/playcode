5910fc2e122663e9d01fdc68ee637aa817130724
SeongmoAhn <asm0619@gmail.com>
2022-12-13 11:13:14 +0900

    var input

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-    int a = 10, b = 20;
-    double c;
-    printf("%d\n", a + b);
-    printf("%d\n", a - b);
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("%f\n", a + b);
+    printf("%f\n", a - b);
     printf("%f\n", a * b);
     printf("%f\n", a / b);
     return 0;
